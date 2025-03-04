eb94dd37de17fe7f1f2ec0c02c2fcba5f021a0af
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-03-04 12:31:51 -0500

    safdsdfs

---
+++ app/src/components/GetData.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue';
+import {useRoute} frp, 'vue-router';
+
+const route = useRoute();
+const crimes = ref("");
+
+async function getCrimes(){
+  let res = await fetch('https://data.cityofnewyork.us/resource/bqiq-cu78.json');
+  let data = await res.json();
+
+  /* https://data.cityofnewyork.us/resource/bqiq-cu78.json */
+}
+</script>
+
+<style scoped>
+
+</style>
