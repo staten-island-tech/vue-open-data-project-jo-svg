106d318072ae2dafd75213b92d2d03224eaed798
jo-svg <Josephinel43@nycstudents.net>
2025-03-16 01:01:44 -0400

    xzc

--- app/src/components/MonthSelector.vue
+++ app/src/components/MonthSelector.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="h-2/3 bg-amber-400">
-    <button class=" ">
+
+    <button class="w-3/5 h-1/13 border-2 bg-transparent text-white hover:text-white hover:shadow-[inset_14rem_0_0_0] hover:shadow-green-500 duration-[400ms,700ms] transition-[color,box-shadow]">
         January
     </button>
+
+
 </div>
 </template>
 
